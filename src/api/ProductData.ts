74dfc1b9d605634df1d63e0c4207c5f31dc9bd82
Bruno <brunoa0504@gmail.com>
2024-04-18 18:15:42 -0300

    feat initial commit

---
+++ src/api/ProductData.ts
@@ -0,0 +1,9 @@
+export type productData = {
+    id: number;
+    name: string;
+    brand: string;
+    description: string;
+    price: string;
+    photo: string;
+    quantity: number;
+};
---
+++ src/app/app.tsx
@@ -0,0 +1,10 @@
+interface AppProps {
+
+}
+export default function App(props : AppProps){
+    return(
+        <main>
+            <h1>Hellow World</h1>
+        </main>
+    )
+}
